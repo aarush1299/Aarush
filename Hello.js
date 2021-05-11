3f500c53ca14a9e88954d23d6804699197ef90a0
aarush1299 <aarushsaini99@gmail.com>
2021-05-11 13:49:35 +0530

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Hello extends Component{
+    render(){
+        return(
+            <div id="root">
+                <h1>
+                    Hello World
+                </h1>
+                <p>
+                    I am learning react.My life is getting better.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
